6e3cebd4c09b9c41e4daea983e8e5c1e1c989116
Achmad Qizwini <akise.shun@gmail.com>
2023-05-23 22:26:03 +0700

    init

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/Achmadqizwini/hackernews
+
+go 1.19
+
+require (
+	github.com/99designs/gqlgen v0.17.31
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-chi/chi v1.5.4
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/vektah/gqlparser/v2 v2.5.1
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	hackernews "github.com/Achmadqizwini/hackernews/graph"
+	"github.com/Achmadqizwini/hackernews/internal/auth"
+	database "github.com/Achmadqizwini/hackernews/internal/pkg/db/migration/mysql"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+	defer database.CloseDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
